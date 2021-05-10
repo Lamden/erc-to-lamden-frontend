aef507e908f845b53c474abb88680d90b5f5d629
jeffwscott <jeffscott@outlook.com>
2021-05-10 17:13:44 -0400

    lamden to eth updates

--- src/components/form-lamden-eth.svelte
+++ src/components/form-lamden-eth.svelte
@@ -1,195 +1,264 @@
 <script lang="ts">
-  import Alert from "../components/alert.svelte";
-  import WalletController from "lamden_wallet_controller";
-  import { vk } from "../stores/lamden";
-  import { web3, selectedAccount } from "svelte-web3";
-  import { projectConf } from "../conf.js";
-  import axios from "axios";
+import Alert from "../components/alert.svelte";
+import WalletController from "lamden_wallet_controller";
+import { vk } from "../stores/lamden";
+import { web3, selectedAccount } from "svelte-web3";
+import { projectConf } from "../conf.js";
+import axios from "axios";
+import BN from 'bignumber.js'
 
-  let isLoading = false;
+let isLoading = false;
+let tokenName = ""
 
-  $: message = "";
-  $: success = "";
+$: message = ""
+$: success = ""
+$: status = ""
 
-  let balance = "";
+let balance = new BN(0);
+let approval = new BN(0);
+let selectedTokenName = ""
 
-  const lamdenClearingHouse = new WalletController(
-    projectConf.lamden.clearingHouse
-  );
+const walletController = new WalletController(
+	projectConf.lamden.clearingHouse
+);
 
-  async function checkBalance(event) {
-    const tokenName = event.target.value;
-    if (tokenName) {
-      const token = projectConf.ethereum.tokens
-        .filter((t) => t.name === tokenName)
-        .pop();
-      try {
-        const res = await fetch(
-          `${projectConf.lamden.network.apiLink}/states/${projectConf.lamden.token.contractName}/balances/${$vk}`,
-          {
-            method: "GET",
-          }
-        );
-        if (res.status === 200) {
-          const value = (await res.json()).value;
-          if (value) {
-            balance = value.__fixed__ + ` ${tokenName}`;
-          } else {
-            balance = `0.0 ${tokenName}`;
-          }
-        }
-      } catch (error) {
-        balance = `0.0 ${tokenName}`;
-      }
-    }
-  }
+async function checkBalance(event) {
+	if (event.target.value) {
+		tokenName = event.target.value;
+		const token = projectConf.ethereum.tokens
+			.filter((t) => t.name === tokenName)
+			.pop();
+		try {
+			const res = await fetch(
+				`${projectConf.lamden.network.apiLink}/states/${projectConf.lamden.token.contractName}/balances/${$vk}`,
+				{
+					method: "GET",
+				}
+			);
+			if (res.status === 200) {
+				const value = (await res.json()).value;
+				if (value) {
+					if (value.__fixed__) balance = new BN(value.__fixed__)
+					else balance = new BN(value)
+				} else {
+					balance = new BN(0);
+				}
+			}
+		} catch (error) {
+			balance = new BN(0);
+		}
+	}
+}
 
-  function startBurn(event) {
-    isLoading = true;
-    message = "";
-    const formData = new FormData(event.target);
-    const tokenName = formData.get("tokenName").toString();
+async function checkApproval() {
+	status = "Checking for Approval...";
+	try {
+		const res = await fetch(
+			`${projectConf.lamden.network.apiLink}/states/${projectConf.lamden.token.contractName}/balances/${$vk}:${projectConf.lamden.clearingHouse.contractName}`,
+			{
+				method: "GET",
+			}
+		);
+		if (res.status === 200) {
+			const value = (await res.json()).value;
+			if (value) {
+				if (value.__fixed__) return new BN(value.__fixed__)
+				else return new BN(value)
+			} else {
+				return new BN(0);
+			}
+		}
+	} catch (error) {
+		return new BN(0);
+	}
+}
 
-    let amount = Number.parseFloat(formData.get("quantity").toString());
+const sendApproval = (amountToApprove) => new Promise(resolve => {
+	status = "Sending Lamden approval (check popup)...";
+	const txInfo = {
+		networkType: projectConf.lamden.clearingHouse.networkType,
+		contractName: projectConf.lamden.token.contractName,
+		methodName: "approve",
+		kwargs: {
+			amount: { "__fixed__": amountToApprove.toFixed(18) },
+			to: projectConf.lamden.clearingHouse.contractName
+		},
+		stampLimit: 65,
+	};
+	walletController.sendTransaction(txInfo, async (txResults) => {
+		if (txResults.status === "success") {
+			try {
+				status = "Lamden approval sent...";
+				resolve(true)
+			} catch (error) {
+				message = 'Transaction failed';
+				resolve(false)
+			}
+		} else {
+			message = txResults.data.resultInfo.errorInfo[0];
+			resolve(false)
+		}
+	});	
+})
 
-    const token = projectConf.ethereum.tokens
-      .filter((t) => t.name === tokenName)
-      .pop();
+const sendBurn = (token, amount) => new Promise(resolve => {
+	const ethereum_contract = token.address;
 
-    if (!token) {
-      isLoading = false;
-      message = "Invalid Token Selected.";
-      return;
-    }
+	const txInfo = {
+		networkType: projectConf.lamden.clearingHouse.networkType,
+		methodName: "burn",
+		kwargs: {
+		ethereum_contract,
+		ethereum_address: $selectedAccount,
+		amount: { "__fixed__": amount.toFixed(18) },
+		},
+		stampLimit: 65,
+	};
 
-    if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
-      isLoading = false;
-      message = "Invalid quantity";
-      return;
-    }
+	walletController.sendTransaction(txInfo, async (txResults) => {
+		if (txResults.status === "success") {
+			try {
+				const unSignedABI = txResults.data.txBlockResult.result;
+				const res = await axios.post(`${window.location.origin}/sign`, {
+					unSignedABI,
+				});
+				const sign = await res.data;
+				const nonce = "0x" + unSignedABI.substring(129, 193);
+				const { v, r, s } = sign;
 
-    const ethereum_contract = token.address;
+				const amountHex = "0x" + unSignedABI.substring(65, 129);
+				const clearingHouseContract = new $web3.eth.Contract(
+					projectConf.ethereum.clearingHouse.abi,
+					projectConf.ethereum.clearingHouse.address
+				);
+				const obj = {
+					unSignedABI,
+					token: token.address,
+					amount: amountHex,
+					nonce: nonce,
+					v,
+					r,
+					s,
+				};
+				const withdrawRes = await clearingHouseContract.methods
+					.withdraw(obj.token, obj.amount, obj.nonce, obj.v, obj.r, obj.s)
+					.send({ from: $selectedAccount });
+				success = 'WETH tokens sent to Ethereum Chain';
+			} catch (error) {
+				message = 'Transaction failed';
+			}
+		} else {
+			message = txResults.data.resultInfo.errorInfo[0];
+		}
+		status = ""
+		isLoading = false;
+	});
+})
 
-    const txInfo = {
-      networkType: projectConf.lamden.clearingHouse.networkType,
-      methodName: "burn",
-      kwargs: {
-        ethereum_contract,
-        ethereum_address: $selectedAccount,
-        amount: amount,
-      },
-      stampLimit: 65,
-    };
-    lamdenClearingHouse.sendTransaction(txInfo, async (txResults) => {
-      if (txResults.status === "success") {
-        try {
-          const unSignedABI = txResults.data.txBlockResult.result;
-          console.log({ unSignedABI });
-          const res = await axios.post(`${projectConf.serverLink}/sign`, {
-            unSignedABI,
-          });
-          const sign = await res.data;
-          console.log(sign);
-          const nonce = "0x" + unSignedABI.substring(129, 193);
-          const { v, r, s } = sign;
+async function startBurn(event) {
+	isLoading = true;
+	message = "";
+	status = "";
+	const formData = new FormData(event.target);
+	const tokenName = formData.get("tokenName").toString();
 
-          const amountHex = "0x" + unSignedABI.substring(65, 129);
-          const clearingHouseContract = new $web3.eth.Contract(
-            projectConf.ethereum.clearingHouse.abi,
-            projectConf.ethereum.clearingHouse.address
-          );
-          const obj = {
-            unSignedABI,
-            token: token.address,
-            amount: amountHex,
-            nonce: nonce,
-            v,
-            r,
-            s,
-          };
-          console.log(obj);
-          const withdrawRes = await clearingHouseContract.methods
-            .withdraw(obj.token, obj.amount, obj.nonce, obj.v, obj.r, obj.s)
-            .send({ from: $selectedAccount });
-          console.log(withdrawRes);
-          isLoading = false;
-          success = 'Buring successful';
-        } catch (error) {
-          console.log(error);
-          message = 'Transaction failed';
-          isLoading = false;
-        }
-      } else {
-        isLoading = false;
-        console.log("Error");
-        console.log(txResults.data.resultInfo.errorInfo);
-        message = txResults.data.resultInfo.errorInfo[0];
-      }
-    });
-  }
+	let amount = new BN(formData.get("quantity").toString());
+
+	const token = projectConf.ethereum.tokens
+		.filter((t) => t.name === tokenName)
+		.pop();
+
+	if (!token) {
+		isLoading = false;
+		message = "Invalid Token Selected.";
+		return;
+	}
+
+	if (typeof amount !== "number" || isNaN(amount) || amount <= 0) {
+		isLoading = false;
+		message = "Invalid quantity";
+		return;
+	}
+
+	let currentApprovalAmount = await checkApproval()
+	if (currentApprovalAmount.isLessThan(amount)){
+		status = "Needs Approval...";
+		if (!await sendApproval(amount)) {
+			isLoading = false;
+			return;
+		}
+	}
+	status = `Sending ${token.name} tokens from Lamden to Ethereum...`
+
+	sendBurn(token, amount)
+}
 </script>
 
 <div class="loading {isLoading ? 'is-loading' : ''}">
-  <h1>Loading</h1>
+	<h1>Loading</h1>
+	<p class="status">{status}</p>
 </div>
 
 <div class="row" style="margin-top: 3rem">
-  <Alert {message} type={"danger"} />
-  <Alert message={success} type={"success"} />
-  <form
-    on:submit|preventDefault={startBurn}
-    action="#"
-    method="POST"
-    style="width: 100%"
-  >
-    <div class="form-group">
-      <label for="tokenName">Token Name</label>
-      <select
-        class="form-control"
-        on:change={checkBalance}
-        name="tokenName"
-        id="tokenName"
-      >
-        <option value="">Select Token</option>
-        {#each projectConf.ethereum.tokens as token}
-          <option value={token.name}>{token.name}</option>
-        {/each}
-      </select>
-      {#if balance}
-        <p>Your balance is: {balance}</p>
-      {/if}
-    </div>
-    <div class="form-group">
-      <label for="quantity">Amount</label>
-      <input
-        type="number"
-        step="0.000000000000000001"
-        class="form-control"
-        name="quantity"
-        id="quantity"
-        placeholder=""
-        min="0"
-      />
-    </div>
-    <br />
+	<Alert {message} type={"danger"} />
+	<Alert message={success} type={"success"} />
+	<form
+		on:submit|preventDefault={startBurn}
+		action="#"
+		method="POST"
+		style="width: 100%">
+		<div class="form-group">
+			<label for="tokenName">Token Name</label>
+			<select
+				class="form-control"
+				on:change={checkBalance}
+				name="tokenName"
+				id="tokenName"
+				>
+				<option value="">Select Token</option>
+				{#each projectConf.ethereum.tokens as token}
+					<option value={token.name}>{token.name}</option>
+				{/each}
+			</select>
+			{#if balance}
+				<p>Your balance is: {`${balance.toFixed(18)} ${tokenName}`}</p>
+			{/if}
+		</div>
 
-    <button type="submit" class="btn btn-outline-primary btn-block"
-      >Begin Burn</button
-    >
-  </form>
+		<div class="form-group">
+			<label for="quantity">Amount</label>
+			<input
+				type="number"
+				step="0.000000000000000001"
+				class="form-control"
+				name="quantity"
+				id="quantity"
+				placeholder=""
+				min="0"/>
+		</div>
+
+		<br />
+
+		<button type="submit" class="btn btn-outline-primary btn-block">
+			Begin Burn
+		</button>
+	</form>
 </div>
 
 <style>
-  .loading {
-    display: none;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(249, 249, 249, 0.7);
-  }
-  .is-loading {
-    display: block;
-  }
+	.loading {
+		display: none;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(249, 249, 249, 0.7);
+	}
+	.is-loading {
+		display: block;
+	}
+	.status{
+		margin: 0
+	}
 </style>
